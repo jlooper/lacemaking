55b9cbaf0840c57be49a639221526ad5b328615a
Jen Looper <jen.looper@gmail.com>
2020-03-14 22:57:45 -0400

    initial addition of notebook, custom transfer learning

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,61 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Inference</h1>
+    <img src="@/assets/cat.jpg" ref="img1" @load="getImage" />
   </div>
 </template>
 
 <script>
+import * as tf from "@tensorflow/tfjs";
+const MODEL_URL = "/models/model.json";
+const classNames = ["cat", "dog"];
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      model: MODEL_URL
+    };
+  },
+  methods: {
+    async loadModel(tensor) {
+      let model = await tf.loadLayersModel(MODEL_URL);
+      this.startInference(tensor, model);
+    },
+    showPrediction(top5) {
+      top5.forEach(function(p) {
+        console.log(p, p.className, p.probability);
+      });
+    },
+    async startInference(tensor, model) {
+      let predictions = await model.predict(tensor).data();
+      console.log(predictions);
+      let top5 = Array.from(predictions)
+        .map(function(p, i) {
+          return {
+            probability: p,
+            className: classNames[i] // we are selecting the value from the obj
+          };
+        })
+        .sort(function(a, b) {
+          return b.probability - a.probability;
+        })
+        .slice(0, 2);
+      this.showPrediction(top5);
+    },
+    getImage() {
+      const image1 = this.$refs.img1;
+
+      let tensor = tf.browser
+        .fromPixels(image1, 3)
+        .resizeNearestNeighbor([160, 160]) // change the image size
+        .expandDims()
+        .toFloat()
+        .reverse(-1); // RGB -> BGR*/
+
+      this.loadModel(tensor);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
