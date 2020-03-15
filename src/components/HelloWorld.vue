fecbc00a7d16b0777de2a253f664215e3d0746d4
Jen Looper <jen.looper@gmail.com>
2020-03-15 12:41:53 -0400

    code update, posting new models

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,77 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>Inference</h1>
-    <img src="@/assets/cat.jpg" ref="img1" @load="getImage" />
-  </div>
+	<div class="hello">
+		<h1>Inference</h1>
+		<img src="@/assets/cheese.jpg" ref="img1" @load="getImage" />
+	</div>
 </template>
 
 <script>
-import * as tf from "@tensorflow/tfjs";
-const MODEL_URL = "/models/model.json";
-const classNames = ["cat", "dog"];
+import * as tf from '@tensorflow/tfjs';
+
+const MODEL_URL = '/models/cheese/model.json';
+//const classNames = ["cat", "dog"];
 export default {
-  name: "HelloWorld",
-  data() {
-    return {
-      model: MODEL_URL
-    };
-  },
-  methods: {
-    async loadModel(tensor) {
-      let model = await tf.loadLayersModel(MODEL_URL);
-      this.startInference(tensor, model);
-    },
-    showPrediction(top5) {
-      top5.forEach(function(p) {
-        console.log(p, p.className, p.probability);
-      });
-    },
-    async startInference(tensor, model) {
-      let predictions = await model.predict(tensor).data();
-      console.log(predictions);
-      let top5 = Array.from(predictions)
-        .map(function(p, i) {
-          return {
-            probability: p,
-            className: classNames[i] // we are selecting the value from the obj
-          };
-        })
-        .sort(function(a, b) {
-          return b.probability - a.probability;
-        })
-        .slice(0, 2);
-      this.showPrediction(top5);
-    },
-    getImage() {
-      const image1 = this.$refs.img1;
+	name: 'HelloWorld',
+	data() {
+		return {
+			//model: MODEL_URL
+		};
+	},
+	methods: {
+		getImage() {
+			//step 1, get the image
+			const image1 = this.$refs.img1;
 
-      let tensor = tf.browser
-        .fromPixels(image1, 3)
-        .resizeNearestNeighbor([160, 160]) // change the image size
-        .expandDims()
-        .toFloat()
-        .reverse(-1); // RGB -> BGR*/
+			let tensor = tf.browser
+				.fromPixels(image1, 3)
+				.resizeNearestNeighbor([160, 160]) // change the image size
+				.expandDims()
+				.toFloat()
+				.reverse(-1);
+			console.log(tensor);
+			this.loadModel(tensor);
+		},
+		async loadModel(tensor) {
+			//step 2, load model, start inference
+			let model = await tf.loadLayersModel(MODEL_URL);
+			this.startInference(tensor, model);
+		},
+		async startInference(tensor, model) {
+			//step 3, inference
+			let prediction = await model.predict(tensor).data();
 
-      this.loadModel(tensor);
-    }
-  }
+			console.log(prediction);
+			let classification = Array.from(prediction);
+			console
+				.log(classification)
+				.map(function(p, i) {
+					return {
+						probability: p,
+						className: prediction[i],
+					};
+				})
+				.sort(function(a, b) {
+					return b.probability - a.probability;
+				})
+				.slice(0, 1);
+			this.showPrediction(classification);
+		},
+		showPrediction(classification) {
+			//step 4 - classify
+			console.log(classification);
+			classification.forEach(function(p) {
+				console.log(p);
+			});
+		},
+	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
